de8fccf44cda63dfb417c29cfed665255b85b360
Pranavprajapat <pranavprajapati762@gmail.com>
2024-08-08 05:45:03 -0700

    Update and rename README.md to even.c

--- even.c
+++ even.c
@@ -3,11 +3,11 @@ int main(){
 
 int x = 4;
 printf("x=%d\n",x);
-int prime = x%2==1;
-printf("x is prime:%d",prime);
+int even = x%2==0;
+printf("x is even:%d",even);
 
 
-return0;
+return 0;
 }
 
 
